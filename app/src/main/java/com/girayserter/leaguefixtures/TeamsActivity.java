27649493c2ef254cc8ac9aa5e9d7c04c4b8bf8dc
Giray <giray_serter@hotmail.com>
2021-05-12 04:53:45 +0300

    Data Binding with TeamsActivity RecycleView, granted Internet permission, created adapter for RecycleView.

--- app/src/main/java/com/girayserter/leaguefixtures/TeamsActivity.java
+++ app/src/main/java/com/girayserter/leaguefixtures/TeamsActivity.java
@@ -1,14 +1,44 @@
 package com.girayserter.leaguefixtures;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 
+import com.girayserter.leaguefixtures.adapters.TeamsAdapter;
+import com.girayserter.leaguefixtures.databinding.ActivityTeamsBinding;
+import com.girayserter.leaguefixtures.models.Team;
+import com.girayserter.leaguefixtures.viewmodels.TeamsViewModel;;
+
+import java.util.List;
+
 public class TeamsActivity extends AppCompatActivity {
+
+    ActivityTeamsBinding binding;
+    TeamsAdapter adapter;
+    List<Team> teams;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teams);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_teams);
+
+        binding.recyclerViewTeams.setHasFixedSize(true);
+        binding.recyclerViewTeams.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter=new TeamsAdapter();
+        binding.recyclerViewTeams.setAdapter(adapter);
+
+        TeamsViewModel teamsViewModel=new TeamsViewModel();
+        teamsViewModel.getTeams().observe(this, new Observer<List<Team>>() {
+            @Override
+            public void onChanged(List<Team> teamList) {
+                adapter.addTeamList(teamList);
+                adapter.notifyDataSetChanged();
+                teams=teamList;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/girayserter/leaguefixtures/adapters/TeamsAdapter.java
@@ -0,0 +1,56 @@
+package com.girayserter.leaguefixtures.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.girayserter.leaguefixtures.R;
+import com.girayserter.leaguefixtures.databinding.ListItemTeamBinding;
+import com.girayserter.leaguefixtures.models.Team;
+import com.girayserter.leaguefixtures.models.Teams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TeamsAdapter extends RecyclerView.Adapter<TeamsAdapter.MyViewHolder> {
+
+    private List<Team> teamList;
+
+
+    public TeamsAdapter(){teamList=new ArrayList<>();}
+
+    public void addTeamList(List<Team> teamList){
+        this.teamList=teamList;
+    }
+
+    @Override
+    public int getItemCount() {
+        return teamList != null ? teamList.size() : 0;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ListItemTeamBinding binding= DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.list_item_team,parent,false);
+        return new MyViewHolder(binding);
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private ListItemTeamBinding binding;
+
+        MyViewHolder(ListItemTeamBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, final int position) {
+        holder.binding.setTeamModel( teamList.get(position) );
+    }
+}
