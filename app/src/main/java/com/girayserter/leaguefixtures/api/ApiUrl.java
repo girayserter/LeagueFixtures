99e473f81bf88ca412e2c7f5d0189d5f7a9c604d
Giray <giray_serter@hotmail.com>
2021-05-12 03:58:50 +0300

    Api Base URL decleration, Created models for Team and Teams, added dependencies for Retrofit

---
+++ app/src/main/java/com/girayserter/leaguefixtures/api/ApiUrl.java
@@ -0,0 +1,5 @@
+package com.girayserter.leaguefixtures.api;
+
+public class ApiUrl {
+    public static final String BASE_URL = "https://run.mocky.io/v3/";
+}
---
+++ app/src/main/java/com/girayserter/leaguefixtures/model/Team.java
@@ -0,0 +1,30 @@
+package com.girayserter.leaguefixtures.model;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Team {
+    @SerializedName("teamId")
+    @Expose
+    private String teamId;
+    @SerializedName("teamName")
+    @Expose
+    private String teamName;
+
+    public String getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(String teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+}
